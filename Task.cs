72b1119c7c5837787d3a1002a45536e330209c13
Q <nhatquang2106qh@gmail.com>
2025-03-07 03:40:54 -0500

    Add project files.

---
+++ Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Task
+    {
+        protected string description;
+
+        public Task()
+        {
+            this.setDescription("Default task");
+        }
+        public Task(string description)
+        {
+            this.setDescription(description);
+        }
+        public string getDescription() { 
+            return description; 
+        }
+        public void setDescription(string description) { 
+            this.description = description;
+        }
+        public void displayTask() // làm hàm này nè
+        {
+        }
+    }
+}
